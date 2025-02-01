e4dae7a03f4d249dc8fef4292487c9f8e6c1cfd3
Bulat Khatypov <hatypov.ba@gmail.com>
2025-02-01 22:24:06 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hello")
+
+
+cats = ["Barsik", "Murzik","Matur"]
+
+print(list(enumerate(cats)))
+
+for idx, name in enumerate(cats):
+    print(f"{idx} . {name}")
+
+print( list(reversed(cats)))
+
+
+eng_dic ={
+    "cat": "кот",
+    "car": "машина"
+}
+for key, value in eng_dic.items():
+    print(f" Ключ: {key} - значение: {value} ")
+symbol_number = ord("а")
+while symbol_number <= ord("я"):
+    print(f"number - {symbol_number} : letter - {chr(symbol_number)}")
+    symbol_number += 1
